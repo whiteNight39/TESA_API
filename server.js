8282e30c941141ad340d2b7ec26d515ad653dfef
Fredrick <gboladefred@gmail.com>
2025-05-23 21:16:51 +0100

    Our first github push

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// Middleware to parse JSON
+app.use(express.json());
+
+
+// Define routes
+app.get('/', (req, res) => {
+  res.send('Hello TESA People!');
+});
+
+
+app.get('/hi', (req, res) => {
+  res.send('Hi there!');
+});
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
